badf2aea0db6a59648efe68f14abf7c413bb3df4
GusFiveO <augustinlorain@gmail.com>
2025-03-04 18:50:44 +0100

    send slack message implemented

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,5 +1,13 @@
+import { WebClient } from "@slack/web-api"
 import { inngest } from "./client";
 import { sampleData } from "./sample";
+
+const token = process.env.NEXT_PUBLIC_SLACK_TOKEN;
+
+// Initialize
+const slackClient = new WebClient(token);
+
+
 
 const getMatch = inngest.createFunction(
 	{id: "create-match"},
@@ -13,4 +21,26 @@ const getMatch = inngest.createFunction(
 	}
 )
 
-export const functions = [getMatch]
+const sendSlackMessage = inngest.createFunction(
+	{id: "send-slack-message"},
+	{event: "slack/message.send"},
+	async ({event}) => {
+		const result = await slackClient.chat.postMessage({
+			channel: process.env.NEXT_PUBLIC_SLACK_CLIENT_ID as string,
+			blocks: [
+			  {
+				type: "section",
+				text: {
+				  type: "mrkdwn",
+				  text: `${event.data.message}`,
+				},
+			  },
+			],
+		  });
+	
+		  return result;
+	
+	}
+)
+
+export const functions = [getMatch, sendSlackMessage]
